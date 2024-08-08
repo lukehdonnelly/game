6dda7b92d2e969e62b62f637bf0883d4f3e82679
Luke Donnelly <lukedonnelly@Lukes-MacBook-Pro-5.local>
2024-08-08 20:50:56 +0930

    added textbox popup

--- scenes/pirateCollision.gd
+++ scenes/pirateCollision.gd
@@ -8,7 +8,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	pass 
 
 
 func _on_area_2d_body_entered(body):
@@ -16,7 +16,4 @@ func _on_area_2d_body_entered(body):
 	var label = Label.new()
 	label.text = "Press E to interact"
 	label.set_position(self.position)
-	var font = FontFile.new()
-	font.size = 24
-	label.add_font_override("font", font)
 	add_child(label)
